769019e843e7f8b787a2483429f531c80b0ede13
alecmr2000 <>
2021-04-19 00:16:08 +0000

    Updating game class

--- Game.java
+++ Game.java
@@ -3,14 +3,16 @@ import java.io.BufferedWriter;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class Game{
+    static ArrayList<Question> thequestions;
     static String fileName;
     static FileReader myFile;
     //Above is declaring variables.
 
     public static void game(String[] args) {
-
+        thequestions = new ArrayList<Question>();
         String question = "";
         String optionA = "";
         String optionB = "";
@@ -24,6 +26,29 @@ public class Game{
 
             BufferedWriter output = new BufferedWriter(toWriteFile);
             //Above is initalizing BufferedWriter.
+
+            for (int i = 0; i < thequestions.size(); i++){
+                output.write(thequestions.get(i).getQuestion());
+                output.newLine();
+                output.write(thequestions.get(i).getOptionA());
+                output.newLine();
+                output.write(thequestions.get(i).getOptionB());
+                output.newLine();
+                output.write(thequestions.get(i).getOptionC());
+                output.newLine();
+                output.write(thequestions.get(i).getOptionD());
+                output.newLine();
+                output.write(thequestions.get(i).getAnswer());
+                output.newLine();
+                output.write(thequestions.get(i).getPoints());
+                output.newLine();
+
+                output.flush();
+            }
+            output.close();
+        }
+        catch (IOException excpt){
+            excpt.printStackTrace();
         }
 
         try{
@@ -40,10 +65,10 @@ public class Game{
                 optionD = reader.readLine();
 
                 System.out.printf(question, optionA, optionB, optionC, optionD);
-                
             }
             reader.close();
-        } catch (IOException excpt){
+        }
+        catch (IOException excpt){
             excpt.printStackTrace();
         }
     }
